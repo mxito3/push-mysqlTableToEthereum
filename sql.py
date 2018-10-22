5d123531bf3183e7e6e04b46ae870a93bec2817f
mxito3 <1477311729@qq.com>
2018-10-22 16:33:29 +0800

    add mysql interface

---
+++ sql.py
@@ -0,0 +1,25 @@
+import pymysql as db
+class Sql:
+    def __init__(self,host,port,user,passwd,db):
+        self.host=host
+        self.port=port
+        self.user=user
+        self.passwd=passwd
+        self.db=db
+
+    def connect(self):
+        self.con=db.connect(host=self.host,port=self.port,user=self.user,passwd=self.passwd,db=self.db)
+        self.cursor = self.con.cursor()
+    def close(self):
+        self.cursor.close()
+        self.con.close()
+    def extractSql(self,command):
+        try:
+            cursor.execute(command)
+            self.con.commit()
+        except:
+            self.connect()
+            cursor = self.con.cursor()
+            cursor.execute(command)
+            self.con.commit()
+        return cursor.fetchall()
